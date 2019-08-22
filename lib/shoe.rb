1836de95dec3690542e8789ffdc2d62395953f7c
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-22 02:42:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,16 +8,13 @@ class Shoe
     @brand = brand
   end
   
- 
-
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
- 
   def brand=(brand)
     @brand = brand  
-    BRANDS << brand 
+    BRANDS << brand unless BRANDS.include?(brand)
   end 
 end
